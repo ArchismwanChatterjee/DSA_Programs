3240bfc0b8c3e5e174746fe1ee27eb2cc7b56e86
Archismwan Chatterjee <archismwancmail@gmail.com>
2023-08-05 02:45:12 +0530

    Added C/ D1

---
+++ C/D1/Array.c
@@ -0,0 +1,7 @@
+/*
+Write a menu driven program for array insertion,deletion,display and exit
+  1) Insertion : Insert an new element in the array at any index where the index is given by user
+  2) Deletion : Delete an element from the array at any index given by the user
+  3) Display : Print all elements of the array
+  4) Exit : Exit from the program
+  */
